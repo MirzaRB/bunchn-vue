824b556c42a437c251e084c985c26005aa3c03fa
MirzaRB <dev.mirzaa.rehan.baig@gmail.com>
2022-06-24 15:11:23 +0500

    added button component

---
+++ src/components/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :class="[
+      buttonClasses,
+      type === 'pill-bordered' ? pillBorderedClasses : '',
+      type === 'fill-rounded' ? fillRoundedClasses : '',
+    ]"
+    @click="$emit('on-press')"
+  >
+    <img v-if="icon ? true : false" :src="icon" />
+    {{ title }}
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: HTMLImageElement,
+      required: false,
+    },
+  },
+  emits: ["on-press"],
+  data() {
+    return {
+      buttonClasses:
+        "flex items-center m-0 px-10 py-2 text-lg text-center font-bold",
+      pillBorderedClasses: "rounded-full border border-black border-solid",
+      fillRoundedClasses: "rounded-lg",
+    };
+  },
+});
+</script>
