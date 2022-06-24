c0e53c24c7b41925321ab44ca74c42c1f3abb042
MirzaRB <dev.mirzaa.rehan.baig@gmail.com>
2022-06-24 19:03:17 +0500

    Card Component Done

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="[cardClass, radius]">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    components: {},
+    props: {
+        rRadius: Boolean,
+        lRadius: Boolean,
+    },
+    data() {
+        return {
+            radius: `${this.rRadius && 'rounded-r-3xl'} ${this.lRadius && 'rounded-l-3xl'}`,
+            cardClass: 'w-1/2 overflow-hidden shadow-card p-8 bg-white mx-auto',
+        }
+    },
+})
+</script>
