9dba9bddb514077924fa3e243cceaa7d48ac682b
MirzaRB <dev.mirzaa.rehan.baig@gmail.com>
2022-06-24 15:12:38 +0500

    refactored app root

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <RouterView />
+  <div class="w-full bg-white max-w-none min-h-screen relative">
+    <Header v-if="isProtected" />
+    <router-view />
+  </div>
 </template>
 
-<style>
-@import '@/assets/css/main.css';
+<script lang="ts">
+import { defineComponent } from 'vue'
 
+import Header from './components/Header.vue'
 
-</style>
+export default defineComponent({
+  components: {
+    Header,
+  },
+  computed: {
+    isProtected() {
+      return this.$route.fullPath === '/login' ? false : true
+    },
+  },
+})
+</script>
