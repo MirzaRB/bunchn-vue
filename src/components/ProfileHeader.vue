bcfabd5e7ec34dfb38a0550a442087464951be77
MirzaRB <106200510+MirzaRB@users.noreply.github.com>
2022-06-27 11:46:23 -0500

    created profile Header (#15)
    * created profile Header
    
    * added title props
    
    * fix: prop typee

---
+++ src/components/ProfileHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="fontSize">
+    {{ title }}
+    <div :class="borderClass" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: {},
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      fontSize: 'text-4xl font-bold ',
+      borderClass: 'border-2 border-solid border-secondary rounded w-16 mt-3 ',
+    }
+  },
+})
+</script>
