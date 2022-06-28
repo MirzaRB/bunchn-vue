b69eb55d0080c46fe64a31432f30f1eb2aed219d
MirzaRB <106200510+MirzaRB@users.noreply.github.com>
2022-06-28 03:30:48 -0500

    fix: container for header (#36)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,64 +3,63 @@
     :class="[generateHeaderClasses]"
     :style="{ transform: isWhiteHeader ? 'translateX(-50%)' : '' }"
   >
-    <router-link
-      to="/"
-      class="w-32"
-    >
-      <img
-        :src="isWhiteHeader ? logo : logoWhite"
-        alt="site-logo"
-      >
-    </router-link>
-    <div
-      v-if="isWhiteHeader"
-      class="flex items-center"
-    >
+    <div class="container flex justify-between mx-auto">
       <router-link
         to="/"
-        class="text-base font-semibold mr-5"
+        class="w-32"
       >
-        Business account?
+        <img
+          :src="isWhiteHeader ? logo : logoWhite"
+          alt="site-logo"
+        >
       </router-link>
-      <Button
-        v-if="globalState.token"
-        title="Log Out"
-        type="pill-bordered"
-        @on-press="logOut"
-      />
-      <router-link
-        v-else
-        to="/login"
+      <div
+        v-if="isWhiteHeader"
+        class="flex items-center"
       >
+        <router-link
+          to="/"
+          class="text-base font-semibold mr-5"
+        >
+          Business account?
+        </router-link>
         <Button
-          title="Log In"
-          type="pill-bordered"
-        />
-      </router-link>
-    </div>
-    <div
-      v-else
-      class="flex items-center"
-    >
-      <router-link
-        to="/dashboard"
-        class="mr-5"
-      >
-        <Button
-          title="Dashboard"
-          type="primary-bordered"
-        />
-      </router-link>
-      <router-link
-        to="/login"
-        class="mr-5"
-      >
-        <Button
+          v-if="globalState.token"
           title="Log Out"
-          type="secondary"
+          type="pill-bordered"
           @on-press="logOut"
         />
-      </router-link>
+        <router-link
+          v-else
+          to="/login"
+        >
+          <Button
+            title="Log In"
+            type="pill-bordered"
+          />
+        </router-link>
+      </div>
+      <div
+        v-else
+        class="flex items-center"
+      >
+        <router-link
+          to="/dashboard"
+          class="mr-5"
+        >
+          <Button
+            title="Dashboard"
+            type="primary-bordered"
+          />
+        </router-link>
+        <router-link to="/login">
+          <Button
+            title="Log Out"
+            type="secondary"
+            @on-press="logOut"
+          />
+        </router-link>
+      </div>
     </div>
   </header>
 </template>
@@ -92,10 +91,10 @@ export default defineComponent({
     },
     generateHeaderClasses() {
       let headerClasses =
-        'w-full flex justify-between px-8 py-6 rounded-bl-3xl rounded-br-3xl'
+        'w-full px-8 py-6 rounded-bl-3xl rounded-br-3xl'
       this.$route.fullPath === '/'
         ? (headerClasses += ' max-w-5xl mx-auto bg-white absolute left-2/4')
-        : (headerClasses += ' max-w-none bg-black')
+        : (headerClasses += ' max-w-none bg-black container')
       return headerClasses
     },
   },
