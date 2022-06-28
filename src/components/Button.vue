84a0b35f01c48ce839834665626ee40db5424c01
MirzaRB <106200510+MirzaRB@users.noreply.github.com>
2022-06-28 10:25:30 -0500

    created secondary bordered btn (#43)

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -5,6 +5,7 @@
       type === 'pill-bordered' ? pillBorderedClasses : '',
       type === 'secondary' ? secondaryClasses : '',
       type === 'primary-bordered' ? primaryBorderedClasses : '',
+      type === 'secondary-bordered' ?secondaryBorderedClasses : '',
       extraClasses ? extraClasses : ''
     ]"
     @click="$emit('on-press')"
@@ -40,7 +41,8 @@ export default defineComponent({
         'm-0 px-10 py-2 text-center border-solid',
       pillBorderedClasses: 'rounded-full border border-black font-bold text-lg',
       primaryBorderedClasses: 'rounded-lg bg-primary text-white text-base font-medium border border-white',
-      secondaryClasses: 'rounded-lg bg-secondary text-white text-base font-medium',
+      secondaryClasses: 'rounded-lg bg-secondary text-white text-base font-montBold',
+      secondaryBorderedClasses: 'rounded-lg bg-white border-2 border-secondary text-secondary text-base font-montBold ',
     }
   },
 })
